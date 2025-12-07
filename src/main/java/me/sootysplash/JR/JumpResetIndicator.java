802d26bd57542e16a7680825a48ad0eae559d509
Alfakynz <alfakynz.nyxaeris@gmail.com>
2025-12-07 08:56:04 +0100

    Port to 1.21.2 - 1.21.10

--- src/main/java/me/sootysplash/JR/JumpResetIndicator.java
+++ src/main/java/me/sootysplash/JR/JumpResetIndicator.java
@@ -1,68 +1,51 @@
 package me.sootysplash.JR;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.render.*;
-import net.minecraft.util.math.ColorHelper;
-import org.joml.Matrix4f;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JumpResetIndicator implements ModInitializer {
-	public static final MinecraftClient mc = MinecraftClient.getInstance();
+    public static final MinecraftClient mc = MinecraftClient.getInstance();
     public static final Logger LOGGER = LoggerFactory.getLogger("jump-reset-indicator");
-	public static int hurtAge, jumpAge;
-	public static long lastModTime;
+    public static int hurtAge, jumpAge;
+    public static long lastModTime;
 
-	@Override
-	public void onInitialize() {
-		HudRenderCallback.EVENT.register((e, t) -> renderWidget(e));
-		LOGGER.info("JumpResetIndicator | Sootysplash was here");
-		AutoConfig.register(ConfigJR.class, GsonConfigSerializer::new);
-	}
-	private void renderWidget(DrawContext context){
-		ConfigJR configJR = ConfigJR.getInstance();
+    @Override
+    public void onInitialize() {
+        HudRenderCallback.EVENT.register((context, tickDelta) -> renderWidget(context));
+        LOGGER.info("JumpResetIndicator | Sootysplash was here");
+        AutoConfig.register(ConfigJR.class, GsonConfigSerializer::new);
+    }
 
-		if(!configJR.enabled)
-			return;
+    private void renderWidget(DrawContext context){
+        ConfigJR configJR = ConfigJR.getInstance();
 
-		String diff = "No Jump";
-		if(lastModTime + 2500 <= System.currentTimeMillis() || Math.abs(jumpAge - hurtAge) >= configJR.ticks) diff = "No Jump";
-		else if (jumpAge == hurtAge + 1) diff = "Perfect!";
-		else if(hurtAge + 1 < jumpAge) diff = "Late: ".concat(String.valueOf(jumpAge - hurtAge + 1)).concat(" Tick");
-		else if(hurtAge + 1 > jumpAge) diff = "Early: ".concat(String.valueOf(hurtAge + 1 - jumpAge)).concat(" Tick");
+        if(!configJR.enabled)
+            return;
 
-		int alpha = 50;
-		int x = configJR.x;
-		int y = configJR.y;
-		int xOffset = 80;
-		int yOffset = 20;
+        String diff = "No Jump";
+        if (lastModTime + 2500 <= System.currentTimeMillis() || Math.abs(jumpAge - hurtAge) >= configJR.ticks) diff = "No Jump";
+        else if (jumpAge == hurtAge + 1) diff = "Perfect!";
+        else if (hurtAge + 1 < jumpAge) diff = "Late: ".concat(String.valueOf(jumpAge - hurtAge + 1)).concat(" Tick");
+        else if (hurtAge + 1 > jumpAge) diff = "Early: ".concat(String.valueOf(hurtAge + 1 - jumpAge)).concat(" Tick");
 
-		if(configJR.background) {
+        int alpha = 50;
+        int x = configJR.x;
+        int y = configJR.y;
+        int xOffset = 80;
+        int yOffset = 20;
 
-			Tessellator tess = Tessellator.getInstance();
-			BufferBuilder bf = tess.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+        if (configJR.background) {
+            int bgColor = (alpha << 24) | (0);
+            context.fill(x, y - yOffset, x + xOffset, y, bgColor);
+        }
 
-			Matrix4f posMat = context.getMatrices().peek().getPositionMatrix();
-
-			bf.vertex(posMat, x, y, 0).color(0, 0, 0, alpha);
-			bf.vertex(posMat, x + xOffset, y, 0).color(0, 0, 0, alpha);
-			bf.vertex(posMat, x + xOffset, y - yOffset, 0).color(0, 0, 0, alpha);
-			bf.vertex(posMat, x, y - yOffset, 0).color(0, 0, 0, alpha);
-
-			RenderSystem.setShader(GameRenderer::getPositionColorProgram);
-			RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
-
-			BufferRenderer.drawWithGlobalProgram(bf.end());
-
-		}
-
-		context.drawCenteredTextWithShadow(mc.textRenderer, diff, (int) (x + (xOffset / 2f)), (int) (y - (yOffset / 1.5f)), ColorHelper.Argb.getArgb(0, 255, 255, 255));
-	}
+        int textColor = 0xFFFFFFFF;
+        context.drawCenteredTextWithShadow(mc.textRenderer, diff, (int) (x + (xOffset / 2f)), (int) (y - (yOffset / 1.5f)), textColor);
+    }
 }
